332868803353c76d19ac81b4fceb2257877fac92
Ivan Vega <ivanvegahdez96@gmail.com>
2025-10-01 07:16:10 +0000

    va a la derecha del nombre

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,7 @@ export default function AboutAndProjectsPage() {
       <div className="mx-auto max-w-4xl mb-12">
         <div className="flex flex-col items-center justify-center gap-8 p-6 bg-card rounded-lg shadow-sm text-center">
             <div className="flex items-center gap-3">
+                <h1 className="font-headline text-4xl font-bold tracking-tight lg:text-5xl">Iván Vega</h1>
                  <Image
                     src="https://storage.googleapis.com/aifirebase/images/k2CaxK7s21g/man-in-suit-posing-for-a-picture-in-a-mirror_1.png"
                     alt="Retrato de Iván Vega"
@@ -27,7 +28,6 @@ export default function AboutAndProjectsPage() {
                     height={40}
                     className="rounded-full object-cover"
                 />
-                <h1 className="font-headline text-4xl font-bold tracking-tight lg:text-5xl">Iván Vega</h1>
             </div>
         </div>
       </div>
